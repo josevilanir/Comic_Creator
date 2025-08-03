ccd5cd36eb5a3797179d4ebd8dc56fd6810866d7
josevilanir <104863215+josevilanir@users.noreply.github.com>
2025-08-03 11:47:06 -0300

    Configure Flask to find top-level templates

--- comic_creator/__init__.py
+++ comic_creator/__init__.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
 
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(
+        __name__, template_folder="../templates", static_folder="../static"
+    )
     app.config.from_object(config_class)
     config_class.init_app(app)
 
     from .routes import bp
     app.register_blueprint(bp)
+
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
     return app
 
